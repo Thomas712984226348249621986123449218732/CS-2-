fc58bb42000abf655b8cd6d2f67f0e5881886bab
Thomas712984226348249621986123449218732 <tbutkiewicz27@gcds.net>
2024-09-10 11:58:39 -0400

    Add files via upload

---
+++ Post Office Code (Thomas).py
@@ -0,0 +1,86 @@
+
+
+
+
+
+print("Welcome to the GCDS Post Office!")
+
+def getZones(zip):
+
+
+    if zip > 00001 and < 06999:
+        skip = 1
+
+    else zip > 07000 and < 19999
+    skip = 2
+
+    if zip > 20000 and < 35999:
+        skip = 3
+
+    return skip
+
+
+def main():
+
+    data = input("Enter height, length, thickness, zipcode1, zipcode2: ")
+
+    dimensions = data.split(",")
+
+
+    height = dimensions[0]
+    length = dimensions[1]
+    thickness = dimensions[2]
+    zipcode1 = dimensions[3]
+    zipcode2 = dimensions[4]
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
